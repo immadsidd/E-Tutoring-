5d7a8c37cf71ff7a1bdcd4714dfd693c6fe59f8d
Immad Siddiqui <immadsiddiqui52@gmail.com>
2023-12-01 23:29:16 +0500

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -5,13 +5,13 @@ $db_password = "ETutoring123!";
 $db_name = "e_tutoring";
 
 // create connection
-$conn = new mysqli($db_host,$db_username,$db_password,$db_name);
-
-
-//check connection
-
-if($conn-> connect_error){
-    die("connection failed");
+// $conn = new mysqli($db_host,$db_username,$db_password,$db_name);
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, $db_host, $db_username, $db_password, $db_name, 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+
 
 ?>
