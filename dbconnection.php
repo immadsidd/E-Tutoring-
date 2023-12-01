0639621d9b321e050ef389d36f0d416fde7c2c30
Immad Siddiqui <immadsiddiqui52@gmail.com>
2023-12-01 17:33:31 +0500

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
-$db_host = "localhost";
-$db_username = "root";
-$db_password = "";
+$db_host = "dbd.mysql.database.azure.com";
+$db_username = "Immad";
+$db_password = "ETutoring123!";
 $db_name = "e_tutoring";
 
 // create connection
