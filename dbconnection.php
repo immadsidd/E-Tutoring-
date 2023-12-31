c297d3d73b90fb705d29253d05761043265e38f9
Immad Siddiqui <immadsiddiqui52@gmail.com>
2023-12-31 20:44:27 +0500

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,17 +1,17 @@
 <?php
-$db_host = "dbd.mysql.database.azure.com";
-$db_username = "Immad";
-$db_password = "ETutoring123!";
+$db_host = "e-tutoring-server.mysql.database.azure.com";
+$db_username = "sspqalqiky";
+$db_password = "4PGD5Z6O4O2P756W$";
 $db_name = "e_tutoring";
 
 // create connection
-// $conn = new mysqli($db_host,$db_username,$db_password,$db_name);
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, $db_host, $db_username, $db_password, $db_name, 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+$conn = new mysqli($db_host,$db_username,$db_password,$db_name);
 
+
+//check connection
+
+if($conn-> connect_error){
+    die("connection failed");
+}
 
 ?>
